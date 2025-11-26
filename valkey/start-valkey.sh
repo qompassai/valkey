96d980128dc135aa80c52fed487f6a99f7840b49
phaedrusflow <map@qompass.ai>
2025-11-25 17:36:54 -0800

    Update 2025-11-25

--- valkey/start-valkey.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# /qompassai/valkey/start-valkey.sh
-# ----------------------------
-# Copyright (C) 2025 Qompass AI, All rights reserved
-cd ~/.config/valkey
-eval $(sops --decrypt secrets.enc.env)
-valkey-server valkey.conf
